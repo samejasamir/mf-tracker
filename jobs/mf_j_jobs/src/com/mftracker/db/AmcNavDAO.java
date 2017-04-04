65f671b713556f9bc5e27b57b966664d13c8c99b
samejasamir <samejasamir@hotmail.com>
2017-04-04 22:29:49 +0530

    Hardened few areas and logic

--- jobs/mf_j_jobs/src/com/mftracker/db/AmcNavDAO.java
+++ jobs/mf_j_jobs/src/com/mftracker/db/AmcNavDAO.java
@@ -1,23 +1,72 @@
 package com.mftracker.db;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
-
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.mftracker.interfaces.IAmcNavDAO;
 
 public class AmcNavDAO implements IAmcNavDAO {
+
+	private DriverManagerDataSource dataSource=null;
+	private Connection connection=null;
 	
-	private JdbcTemplate _jdbcTemplate=null;
+	public AmcNavDAO(DriverManagerDataSource dataSource) {		
+		this.dataSource = dataSource;
+	}
 	
-	public AmcNavDAO(JdbcTemplate jdbcTemplate) {
-		_jdbcTemplate = jdbcTemplate;
+	public Connection OpenConnection()	
+	{
+		try {
+			this.connection = dataSource.getConnection();
+			return this.connection;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void CloseConnection()
+	{
+		try {
+			if(this.connection != null && !this.connection.isClosed())
+				this.connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void AddNewNAV(List<String> nav) {
+	public int AddNewAMC(String amc_name) throws SQLException {
 		
-		String insertStatement = "";
+		CallableStatement callableStatement = connection.prepareCall("call AddAMC(?)");
+		callableStatement.setString(1, amc_name);
+		ResultSet result = callableStatement.executeQuery();
+		result.next();
+		return result.getInt(1);		
+	}
+
+
+	@Override
+	public void AddNewNAV(int amc_id, String schemeName, String schemeISIN, String schemeCode, String schemeType,
+			java.util.Date navDated, double navPrice, double navRepurchacePrice, double navSalesPrice) throws SQLException {
+		
+		
+		CallableStatement callableStatement = connection.prepareCall("call AddNAV(?,?,?,?,?,?,?,?,?)");
+		callableStatement.setInt(1, amc_id);
+		callableStatement.setString(2, schemeName);
+		callableStatement.setString(3, schemeISIN);
+		callableStatement.setString(4, schemeCode);
+		callableStatement.setString(5, schemeType);
+		callableStatement.setDate(6, new java.sql.Date(navDated.getTime()));
+		callableStatement.setDouble(7, navPrice);
+		callableStatement.setDouble(8, navRepurchacePrice);
+		callableStatement.setDouble(9, navSalesPrice);		
+		callableStatement.executeUpdate();
 	}
 
 }
--- jobs/mf_j_jobs/src/com/mftracker/implementation/SyncLatestNAV.java
+++ jobs/mf_j_jobs/src/com/mftracker/implementation/SyncLatestNAV.java
@@ -1,5 +1,6 @@
 package com.mftracker.implementation;
 
+import com.mftracker.interfaces.IAmcNavDAO;
 import com.mftracker.interfaces.ISyncLatestNAV;
 
 import java.awt.List;
@@ -13,16 +14,43 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 
 
-public class SyncLatestNAV implements ISyncLatestNAV {
+public class SyncLatestNAV implements ISyncLatestNAV {	
+	
+	private IAmcNavDAO dao=null;
+	
+	public SyncLatestNAV() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public SyncLatestNAV(IAmcNavDAO dao) {
+		this.dao = dao;
+	}
 
 	@Override
-	public void Sync()
+	public void Sync() 
 	{
-		java.util.List<String> output = GetLatestNAVFromAMFII();
-		UpdateNAVs(output);
-		//System.out.println(output);
+		//METRICS INFO
+		java.util.Date startTime = new java.util.Date();
+		
+		try
+		{			
+			java.util.List<String> output = GetLatestNAVFromAMFII();
+			UpdateNAVs(output);
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}		
+		finally
+		{
+			long diff = ((new java.util.Date()).getTime() - startTime.getTime());
+			System.out.println("COMPLETED SYNC IN " + diff + " MILLISECONDS" );
+		}
 	}
 	
 	private java.util.List<String> GetLatestNAVFromAMFII()
@@ -31,8 +59,22 @@ public class SyncLatestNAV implements ISyncLatestNAV {
 		try {
 			websiteUrl = new URL("https://www.amfiindia.com/spages/NAVAll.txt");
 			ReadableByteChannel rbc =  Channels.newChannel(websiteUrl.openStream());
-			FileOutputStream fos = new FileOutputStream("latestnav");
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			
+			FileOutputStream fos = null;
+			try
+			{				
+				fos = new FileOutputStream("latestnav");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);				
+			}
+			finally
+			{
+				if(fos != null)
+				{
+					fos.flush();			
+					fos.close();
+				}
+			}			
+			
 			return  Files.readAllLines(Paths.get("latestnav"));						
 			
 		} catch (Exception e) {
@@ -41,39 +83,62 @@ public class SyncLatestNAV implements ISyncLatestNAV {
 		}		
 	}
 	
-	private void UpdateNAVs(java.util.List<String> listNavs)
+	private void UpdateNAVs(java.util.List<String> listNavs) throws SQLException
 	{
 		String schemeType="";
-		String schemeName="";
+		String amcName="";
+		int amcId = 0;		
 		
-		for(String nav:listNavs)
+		listNavs.remove(0);
+		
+		try
 		{
-			String[] columns = nav.split(";");
-			if(columns.length == 1)
-			{
-				if(columns[0] != null && !columns[0].trim().isEmpty())
+			dao.OpenConnection();
+			for(String nav:listNavs)
+			{	
+				String[] columns = nav.split(";");
+				if(columns.length == 1)
 				{
-					//COLLECTING SCHEME TYPE
-					if(columns[0].contains("Schemes") && schemeType != columns[0])
+					if(columns[0] != null && !columns[0].trim().isEmpty())
 					{
-						schemeType = columns[0];
-						System.out.println(schemeType);
-					}
-					//INITIALIZING SCHEME NAME 
-					else if(schemeName != columns[0])
+						//COLLECTING SCHEME TYPE
+						if(columns[0].contains("Schemes") && schemeType != columns[0])
+						{
+							schemeType = columns[0];
+							System.out.println(schemeType);
+						}
+						//INITIALIZING SCHEME NAME 
+						else if(amcName != columns[0])
+						{
+							amcName = columns[0];
+							amcId = dao.AddNewAMC(amcName);
+							System.out.println(amcId);
+							System.out.println(amcName);
+						}
+					}				
+					
+				}
+				else
+				{
+					try
 					{
-						schemeName = columns[0];
-						System.out.println(schemeName);
+						dao.AddNewNAV(amcId, columns[3], columns[1], columns[0], schemeType,new java.util.Date(),
+								Double.parseDouble(columns[4]), Double.parseDouble(columns[5]), Double.parseDouble(columns[6]));
+						
+						System.out.println(columns[3]);
 					}
-				}				
+					catch(Exception ex)
+					{
+						System.out.println("COULDNT ADD NAV FOR " + amcName + " " + columns + " " + ex.getMessage());
+					}
+				}
 				
 			}
-			else
-			{
-				//CALL DAO
-			}
-			
-		}		
+		}
+		finally		
+		{
+			dao.CloseConnection();			
+		}
 	}
 	
 }
